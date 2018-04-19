cc1e9292c74e8e39227082016e1b93d3ac0f250c
lorraine rono <lorrainerono2017@gmail.com>
2018-04-19 16:04:03 +0300

    added the quotes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Quotes}from './quotes'
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+    quotes = [
+      new Quotes (1,'I dont trust easily but when i trust you dont make me regret it.'),
+      new Quotes (2,'Ive been in love its painful pointless and overrated'),
+      new Quotes ( 3,'if youre going to be bad be bad with a reason or else youre not worth forgiving.'),
+      new Quotes (4,'Our fate lives within us.We just need to be brave to see it.'),
+      new Quotes (5,'humanity is my greatest weakness'),
+      new Quotes (6,'When you lose someone it stays with you.Always reminding you of how easy it is to get hurt'),
+      new Quotes (7,'The worst day of loving someone us the day you lose them'),
+      new Quotes (8,'The day you say you started living is the day you do the thing youre mostly afraid of'),
+
+    ]
 }
